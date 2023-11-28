8c6bd6763b7ef6951b718437c6a7a51f96afee3c
Kolbeyj <kolbey.jones@selu.edu>
2023-11-28 12:43:05 -0600

    Neptunes Grades are GOOD

--- src/GradeSimulator.java
+++ src/GradeSimulator.java
@@ -1,10 +1,48 @@
 public class GradeSimulator {
-    /* Attributes/Data fields/Properties */
-    // YOUR CODE HERE
+    private double totalAssignmentGrade;
+    private double totalMaxAssignmentGrade;
+    private double totalExamGrade;
+    private double totalMaxExamGrade;
 
-    /* Constructor(s) */
-    // YOUR CODE HERE
+    public GradeSimulator() {
+        totalAssignmentGrade = 0;
+        totalMaxAssignmentGrade = 0;
+        totalExamGrade = 0;
+        totalMaxExamGrade = 0;
+    }
 
-    /* Method(s) */
-    // YOUR CODE HERE
+    public void addQuizOrAssignment(double myGrade, double maxGrade) {
+        if (myGrade >= 0 && maxGrade > 0) {
+            totalAssignmentGrade += myGrade;
+            totalMaxAssignmentGrade += maxGrade;
+        }
+    }
+
+    public void addExam(double myGrade, double maxGrade) {
+        if (myGrade >= 0 && maxGrade > 0) {
+            totalExamGrade += myGrade;
+            totalMaxExamGrade += maxGrade;
+        }
+    }
+
+    public double getFinalGrade() {
+        double assignmentPercentage = (totalMaxAssignmentGrade > 0) ? totalAssignmentGrade / totalMaxAssignmentGrade : 0;
+        double examPercentage = (totalMaxExamGrade > 0) ? totalExamGrade / totalMaxExamGrade : 0;
+        double finalGrade = (assignmentPercentage + examPercentage) / 2;
+        finalGrade = Math.max(0, Math.min(1, finalGrade));
+        return finalGrade * 100;
+    }
+
+    public static void main(String[] args) {
+        GradeSimulator gradeSimulator = new GradeSimulator();
+
+        gradeSimulator.addQuizOrAssignment(90.0, 100.0);
+        gradeSimulator.addQuizOrAssignment(80.0, 100.0);
+
+        gradeSimulator.addExam(75.0, 100.0);
+        gradeSimulator.addExam(85.0, 100.0);
+
+        double finalGrade = gradeSimulator.getFinalGrade();
+        System.out.println("Final Grade: " + finalGrade);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        GradeSimulator grader = new GradeSimulator();
+        GradeSimulator gradeSimulator = new GradeSimulator();
 
-        grader.addQuizOrAssignment(1, 1); // Attendance quizzes
-        grader.addQuizOrAssignment(1, 1);
-        grader.addQuizOrAssignment(1, 1);
-        grader.addQuizOrAssignment(1, 1);
-        grader.addQuizOrAssignment(1, 1);
+        gradeSimulator.addQuizOrAssignment(90.0, 100.0);
+        gradeSimulator.addQuizOrAssignment(80.0, 100.0);
 
-        grader.addQuizOrAssignment(5, 5); // Hello assignment
-        grader.addQuizOrAssignment(5, 5); // Second assignment
-        grader.addQuizOrAssignment(10, 10); // Third assignment
+        gradeSimulator.addExam(75.0, 100.0);
+        gradeSimulator.addExam(85.0, 100.0);
 
-        grader.addExam(40, 40); // I nailed the first exam
-        grader.addExam(30, 40); // I did well in the second exam
-        grader.addExam(0, 40); // I bombed in the third exam
-
-        System.out.println("My grade before the final: " + grader.getFinalGrade());
-
-        grader.addExam(40, 40); // I nailed the final exam
-
-        System.out.println("My grade after the final: " + grader.getFinalGrade());
+        double finalGrade = gradeSimulator.getFinalGrade();
+        System.out.println("Final Grade: " + finalGrade);
     }
 }
